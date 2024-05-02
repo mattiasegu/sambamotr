7b636328a373c5e2f626738c65f769f34b47c87d
mattiasegu <mattia.segu@gmail.com>
2024-05-02 20:59:36 +0200

    Script raven

--- scripts/hpc/raven/train_sambamotr.sh
+++ scripts/hpc/raven/train_sambamotr.sh
@@ -2,6 +2,13 @@
 DATASET=dancetrack
 JOB_NAME=${DATASET}/debug_sambamotr_masking_sync
 CONFIG=configs/sambamotr/${DATASET}/def_detr/train_masking_sync.yaml
+
+# rescale for NUM_SAMPLES=4
+LR=0.0001
+LR_BACKBONE=0.00001
+LR_POINTS=0.000005
+
+JOB_NAME=${JOB_NAME}_lr_${LR}
 OUT_DIR=/ptmp/msegu/workspaces/sambamotr/outputs/tmp/${JOB_NAME}/
 BS=1 
 DATA_ROOT=/ptmp/msegu/data/
@@ -10,6 +17,8 @@ GPUS=1
 # GPUS=2
 CMD=scripts/hpc/raven/tools/slurm_train.sh
 
+
+PORT=$(shuf -i 24000-29500 -n 1)
 ${CMD} \
 ${PORT} \
      --config-path ${CONFIG} \
